8bdbb11df3bace356c0db12f19967299f7e71662
Rony <rbnedkov@gmail.com>
2020-05-06 16:51:36 +0200

    merge

--- soil_sensor.py
+++ soil_sensor.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python
 import time
 import psycopg2
+import busio
+from board import SCL, SDA
+from adafruit_seesaw.saw import Seesaw
 
 # connnect to database
-con = psycopg2.connect(database='', user='')
+con = psycopg2.connect(database='', user='', password="", host='localhost')
 cur = con.cursor()
- 
-from board import SCL, SDA
-import busio
- 
-from adafruit_seesaw.seesaw import Seesaw
  
 i2c_bus = busio.I2C(SCL, SDA)
  
 ss = Seesaw(i2c_bus, addr=0x36)
  
-while True:
-    # read moisture level through capacitive touch pad
-    moisture = ss.moisture_read()
+# read moisture level through capacitive touch pad
+moisture = ss.moisture_read()
  
-    # read temperature from the temperature sensor
-    temp = ss.get_temp()
+# read temperature from the temperature sensor
+temp = ss.get_temp()
  
-    print("temp: " + str(temp) + "  moisture: " + str(touch))
-    cur.execute("INSERT INTO  raspi (TEMP, HUMIDITY) VALUES (%s, %s)", (temp, moisture))
-    con.commit()
-    time.sleep(60)
+print("temp: " + str(temp) + "  moisture: " + str(moisture))
+cur.execute("INSERT INTO  soil_measurements (TEMP, MOISTURE) VALUES (%s, %s)", (temp, moisture))
+con.commit()
+#time.sleep(60)
 
