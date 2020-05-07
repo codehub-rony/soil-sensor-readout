ca8e7a534a420cb7430f80a6d6ceda316922e5f6
Rony <rbnedkov@gmail.com>
2020-05-07 11:31:38 +0200

    Set moisture read out to 15 times. Rounding the temp

--- soil_sensor.py
+++ soil_sensor.py
@@ -6,21 +6,21 @@ from board import SCL, SDA
 from adafruit_seesaw.seesaw import Seesaw
 
 # connnect to database
-con = psycopg2.connect(database='', user='', password="", host='localhost')
+con = psycopg2.connect(database='', user='', password='', host='localhost')
 cur = con.cursor()
  
 i2c_bus = busio.I2C(SCL, SDA)
 ss = Seesaw(i2c_bus, addr=0x36)
  
-# read moisture level through capacitive touch pad 10 times and take average to eliminate measure error
+# read moisture level several times and take average to limit measure error
 moisture_measurements = 0
-for i in range(10):
- moisture_measurements = moisture_measurements + ss.moisture_read())
+for i in range(15):
+ moisture_measurements = moisture_measurements + ss.moisture_read()
  
-moisture = round((moisture_measurements / 10), 0)
+moisture = round((moisture_measurements / 15))
  
 # read temperature from the temperature sensor
-temp = ss.get_temp()
+temp = round(ss.get_temp(),1)
  
 print("temp: " + str(temp) + "  moisture: " + str(moisture))
 cur.execute("INSERT INTO  soil_measurements (TEMP, MOISTURE) VALUES (%s, %s)", (temp, moisture))
