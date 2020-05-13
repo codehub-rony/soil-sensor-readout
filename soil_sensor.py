0f5f9843a705737c00d9f43cf0249eb209558945
Rony <rony@gisspecialisten.nl>
2020-05-13 09:10:31 +0200

    Fixed intendion

--- soil_sensor.py
+++ soil_sensor.py
@@ -20,11 +20,11 @@ def waterPlant():
 	time.sleep(2)
 	for i in range(4):
 		GPIO.output(37, True)
-	 	print('switching pump: ON')
-	 	time.sleep(10)
-	 	GPIO.output(37, False)
-	 	print('pump: OFF')	
-	 	time.sleep(5)
+		print('switching pump: ON')
+		time.sleep(10)
+		GPIO.output(37, False)
+		print('pump: OFF')	
+		time.sleep(5)
 
 
 	GPIO.cleanup()
