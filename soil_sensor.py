a928cd2f19ab730dee8b99265040c66b6de7599d
Rony <rony@gisspecialisten.nl>
2020-05-13 09:30:56 +0200

    removed setting of board

--- soil_sensor.py
+++ soil_sensor.py
@@ -11,22 +11,17 @@ import datetime
 
 def waterPlant():
 	print(GPIO.getmode())
-	# if GPIO.getmode() != GPIO.BOARD:
-	# 	GPIO.setmode(GPIO.BOARD)
-
-	# GPIO.setup(37, GPIO.OUT)
+	GPIO.setup(37, GPIO.OUT)
 	
-	# time.sleep(2)
-	# for i in range(4):
-	# 	GPIO.output(37, True)
-	# 	print('switching pump: ON')
-	# 	time.sleep(10)
-	# 	GPIO.output(37, False)
-	# 	print('pump: OFF')	
-	# 	time.sleep(5)
+	time.sleep(2)
+	for i in range(4):
+		GPIO.output(37, True)
+		print('switching pump: ON')
+		time.sleep(10)
+		GPIO.output(37, False)
+		print('pump: OFF')	
+		time.sleep(5)
 
-
-	# GPIO.cleanup()
 	print('DONE')
 
 # Current plant that is being monitored
